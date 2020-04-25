415ac22fef59db77fe08cc65d81e007dfcbdd4c2
感觉好想睡觉 <18721912858@126.com>
2020-04-25 15:18:26 +0800

    update resources

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioManager audioManager;
+
+    private AudioSource musicAudio, playerAudio, fxAudio, ambientAudio;
+
+    private void Awake()
+    {
+        if(audioManager == null)
+        {
+            audioManager = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        musicAudio = gameObject.AddComponent<AudioSource>();
+        playerAudio = gameObject.AddComponent<AudioSource>();
+        fxAudio = gameObject.AddComponent<AudioSource>();
+        ambientAudio = gameObject.AddComponent<AudioSource>();
+
+        musicAudio.clip = Resources.Load<AudioClip>("Sounds/BGM");
+        musicAudio.playOnAwake = false;
+        // musicAudio.Play();
+        ambientAudio.clip = Resources.Load<AudioClip>("Sounds/Ambient");
+        ambientAudio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
